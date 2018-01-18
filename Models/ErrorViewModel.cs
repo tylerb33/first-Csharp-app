f81ef79b0e8f83393e11f4a285a9a5c05c33b4c2
Tyler Barnett <tyler.barnett7190@gmail.com>
2018-01-18 14:58:50 -0600

    first csharp project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MvcMovie2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie2.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie2.Models.Movie> Movie { get; set; }
+    }
+}
